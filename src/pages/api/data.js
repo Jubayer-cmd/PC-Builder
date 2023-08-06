88b93bb61e00d8a6a2186fb7c189e09ea7639964
Jubayer-cmd <sheikhabujubayer@gmail.com>
2023-08-06 22:52:06 +0600

    env uri

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://jubayer:jubayer98@cluster0.qygv5.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.DATABASE_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
